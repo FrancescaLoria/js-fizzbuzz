f5058a05677fb701279a1f9f13c22a6c23ff531b
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-10 15:32:27 +0100

    ciclo da 1 a 100

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+//  Scrivi un programma che stampi in console i numeri da 1 a 100,
+//  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+//  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// PARTIAMO COL COSTRUIRE UN CIGLO DA 1 A 100.
+for (let i = 1; i < 101; i++) {
+  console.log(i);
+}
