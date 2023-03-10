ebfbacdd1d82e2622eb842f613a927e092bef5d5
FrancescaLoria <francesca.loria90@hotmail.it>
2023-03-10 16:00:22 +0100

    completed exercise

--- JS/script.js
+++ JS/script.js
@@ -4,5 +4,12 @@
 
 // PARTIAMO COL COSTRUIRE UN CIGLO DA 1 A 100.
 for (let i = 1; i < 101; i++) {
-  console.log(i);
+  // CONTROLLO CHE IL NUMERO SIA DIVISIBILE PER 3 E PER 5, SE RISULTA FALSO, CONTROLLO PRIMA PER 3 E POI PER 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbazz", i);
+  } else if (i % 3 === 0) {
+    console.log("frizz", i);
+  } else if (i % 5 === 0) {
+    console.log("bazz", i);
+  }
 }
